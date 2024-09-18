f5d2321efa2edf77e810fd1f51a63d3d2492df76
Rimao416 <kayumbaomari27@gmail.com>
2024-09-18 16:29:07 +0200

    jour8

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ### Le jour 6, l'animation était faite par un drag & drop
 ### Le Jour 7, l'animation se fait automatiquement
+### Le jour 8, l'animation se fera en cliquant sur le carré
 Dans ce deuxième jour, nous essayons de faire une simple animation 360 Degré
 ![Image de l'animation](/src/assets/jour_6_7.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import { motion,useTransform,useMotionValue } from "framer-motion";
+import {
+  motion,
+  useTransform,
+  useMotionValue,
+  useAnimate,
+} from "framer-motion";
 import "./App.css";
 
 function App() {
-  const x=useMotionValue(0)
+  const [scope, animate] = useAnimate();
+  function sequence() {
+    animate(scope.current, { x: [0, 100, 0, -100, 0] }, {
+      repeat: Infinity,
+      repeatType: "loop", // Utilisation de "loop" pour répéter le cycle complet
+      duration: 2, // Durée d'une boucle complète
+    });
+  }
+
+  const x = useMotionValue(0);
   const background = useTransform(
     x,
     [-100, 0, 100],
     ["#a0d", "rgba(0,0,0,0)", "#0bf"]
-);
+  );
 
-
-  
   return (
     <>
-      <motion.div className="App"
-      style={{backgroundColor:background}}
-      >
-        <motion.div className="center"
-        
-        style={{x}}
-        animate={{x:[-100,0,100],
-          transition:{
-            repeat:Infinity,
-            repeatType:'reverse',
-            repeatDelay:1,
-          
-          }
-        }}
+      <motion.div className="App" style={{ backgroundColor: background }}>
+        <motion.div
+          className="center"
+          style={{ x }}
+          ref={scope}
+          onClick={sequence}
         ></motion.div>
       </motion.div>
     </>
