0271b25d0d852e0eae7715e464f00c033379ad01
Rimao416 <kayumbaomari27@gmail.com>
2024-09-18 15:35:58 +0200

    Jour5

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Jour 2: Animate
+# Jour 5: Applying aspect  ration with framer motion
 
 Dans ce deuxième jour, nous essayons de faire une simple animation 360 Degré
-![Image de l'animation](/src/assets/jour_2.png)
+![Image de l'animation](/src/assets/jour_5.png)
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,71 @@
 import { motion } from "framer-motion";
 import "./App.css";
+import { useState } from "react";
+
+interface Aspect {
+  width: number;
+  height: number;
+}
 
 function App() {
+  const [aspect, setAspect] = useState<Aspect>({
+    width: 200,
+    height: 2,
+  });
+
+  const onWidthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAspect((prev) => ({
+      ...prev,
+      width: Number(e.target.value),
+    }));
+  };
+
+  const onHeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAspect((prev) => ({
+      ...prev,
+      height: Number(e.target.value),
+    }));
+  };
+
   return (
     <>
       <div className="App">
-        <motion.div className="center"
-        initial={{scale:0}}
-        animate={{scale:1, rotate:360}}
-        transition={{duration:0.5,
-          delay:1,
-        }}
-        ></motion.div>
+        <motion.div
+          className="center"
+          animate={{
+            width: aspect.width + "px",
+            height: aspect.height * 100 + "px",
+          }}
+          transition={{
+            type: "spring",
+
+            stiffness: 50,
+            damping: 10,
+            // delay: 0.5,
+          }}
+          style={{
+            backgroundColor: "lightblue",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          Contenu
+        </motion.div>
+        <div className="top">
+          <input
+            type="number"
+            placeholder="Hauteur (ratio)"
+            defaultValue={aspect.height}
+            onChange={onHeightChange}
+          />
+          <input
+            type="number"
+            placeholder="Largeur"
+            defaultValue={aspect.width}
+            onChange={onWidthChange}
+          />
+        </div>
       </div>
     </>
   );
