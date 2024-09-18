6879f572954848262a57a7e19b4239a29724bae4
Rimao416 <kayumbaomari27@gmail.com>
2024-09-18 09:37:09 +0200

    00_Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Salut les aùùos</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
