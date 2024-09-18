d2e2e1596161d1a748301639028bd0aff2d61cf6
Rimao416 <kayumbaomari27@gmail.com>
2024-09-18 10:05:53 +0200

    Ajout pédagogique

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,13 @@ function App() {
                 initial={{ opacity: 0,scale:0.75 }}
                 animate={{opacity:1,scale:1}}
                 exit={{opacity:0,scale:0}}
+                // Cette partie est purement pédagogique
+                transition={{
+                  bounce:5,
+                  ease:"easeIn",
+                  duration:0.5,
+
+                }}
               ></motion.div>
             )}
           </AnimatePresence>
